3203db66ee0fc6d67285626d7e910c7817873ad8
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-09-26 21:24:42 +0700

    Create Latihan2.java

---
+++ Modul4/Latihan2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        int nilai1 = 10;
+        int nilai2 = 20;
+        int nilai3 = 45;
+        
+        System.out.println("number 1 = " + nilai1);
+        System.out.println("number 2 = " + nilai2);
+        System.out.println("number 3 = " + nilai3);
+        System.out.println("Average is = " + ((nilai1 + nilai2 + nilai3)/3));
+    }
+}
