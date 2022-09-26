d207174af28838748c2dc61e8497700ae0d5869f
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-09-26 21:19:48 +0700

    Create Latihan2.java

---
+++ Modul3/Latihan2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        System.out.println("I think that I shall never see, ");
+        System.out.println("a poem as lovely as a tree. ");
+        System.out.println("A tree whose hungry mouth is pressed");
+        System.out.println("Against the Earth’s sweet flowing breast.");
+    }
+}
