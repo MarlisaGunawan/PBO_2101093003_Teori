5152fa4accc7f6a25e7d9176a50425e3f63167c7
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-03 13:36:15 +0700

    Create Latihan6.java

---
+++ Modul6/Latihan6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan6 {
+    public static void main(String[] args){
+        int x = 0;
+        do
+        {
+            System.out.println("Lisa");
+            x++;
+        }while(x<=100);
+    }
+}
