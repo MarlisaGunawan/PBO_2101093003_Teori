f0eb90ee343821f226a1fda1e2f765fe0783e990
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-03 13:36:13 +0700

    Create Latihan4.java

---
+++ Modul6/Latihan4.java
@@ -0,0 +1,37 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan4 {
+    public static void main(String[] args){
+        String angka="";
+        
+        angka =JOptionPane.showInputDialog("Masukan Angka :");
+        int z = Integer.valueOf(angka).intValue();
+         
+        String hasil="";
+        switch(z){
+            case 1: hasil+= "Valid Number"; break;
+            case 2: hasil+= "Valid Number"; break;
+            case 3: hasil+= "Valid Number"; break;
+            case 4: hasil+= "Valid Number"; break;
+            case 5: hasil+= "Valid Number"; break;
+            case 6: hasil+= "Valid Number"; break;
+            case 7: hasil+= "Valid Number"; break;
+            case 8: hasil+= "Valid Number"; break;
+            case 9: hasil+= "Valid Number"; break;
+            case 10: hasil+= "Valid Number"; break;
+            default: hasil+= "Invalid Number";break;
+        }
+        JOptionPane.showMessageDialog(null,hasil);
+    }
+}
