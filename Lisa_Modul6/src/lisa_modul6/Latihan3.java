dfab8add85f3e169bd26e0dc8e6e7f2b5423b8d4
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-12 06:58:10 +0700

    Create Latihan3.java

---
+++ Lisa_Modul6/src/lisa_modul6/Latihan3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lisa_modul6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan3 {
+    public static void main(String[] args){
+        String angka="";
+        
+         angka =JOptionPane.showInputDialog("Masukan Angka :");
+         int z = Integer.valueOf(angka).intValue();
+         
+         String hasil="";
+         if (z>=1 && z<=10){
+             hasil += "Valid Number";
+         }else{
+             hasil += "Invalid Number";
+         }
+         JOptionPane.showMessageDialog(null, hasil);
+    }
+}
