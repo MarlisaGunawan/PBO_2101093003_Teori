b3b1161c03c3a1cafffc65e5a06790eefbab85b6
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-12 06:58:23 +0700

    Create Latihan7.java

---
+++ Lisa_Modul6/src/lisa_modul6/Latihan7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lisa_modul6;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan7 {
+    public static void main(String[] args){
+        int i;
+        for(i=0; i<=100; i++){
+            System.out.println("Lisa");
+        }
+    }
+}
